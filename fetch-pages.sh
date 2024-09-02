ed8c3b66d82d4b2e458ee99d00adf971af248410
Ed Harrison <eharrison2@apple.com>
2024-09-01 22:24:55 -0700

    Update script to reflect page renaming/renumbering.

--- fetch-pages.sh
+++ fetch-pages.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
-curl "https://images.nypl.org/index.php?id=1945[636-734]&t=w" -o page_#1.jpeg
+curl "https://images.nypl.org/index.php?id=1945[636-734]&t=w" -o page_#1.jpegs
+
+mv page_734.jpeg _0_back_cover.jpeg
+mv page_636.jpeg _1_front_cover.jpeg
+
+mv page_637.jpeg _i_title_page.jpeg
+mv page_638.jpeg _ii_cover_page.jpeg
+mv page_639.jpeg _iii_copyright_page.jpeg
+mv page_640.jpeg _iv_dedication_page.jpeg
+
+for ((i=641;i<=733;i++)); do
+
+    j=$(( $i - 640))
+
+    echo "mv page_$i.jpeg page_$j.jpeg"
+
+done
